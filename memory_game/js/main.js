a9d5e43ed8e3f4d4ee9bebc4fbab16479b07fe40
Peter Lutz <jordanghannam@Jordans-MacBook-Pro.local>
2019-11-27 10:41:10 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne="queen";
+var cardTwo="queen";
+var cardThree="king";
+var cardFour="king";
+
+console.log("User Flipped "+cardFour);
+console.log("User flipped "+cardOne);
