592c1b30a6e0023b1c688e66aef9be6e3a055ab4
Peter Lutz <jordanghannam@Jordans-MacBook-Pro.local>
2019-11-27 11:48:47 -0500

    created functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,18 +4,24 @@ var cards = ["queen","queen","king","king"];
 
 var cardsInPlay=[];
 
-var cardOne=cards[0];
-var cardTwo=cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length ===2){
-    if(cardsInPlay[0] === cardsInPlay[1]){
-        alert("You've found a match");
+function flipCard(cardID){
+    console.log("User flipped "+cards[cardID]);
+    cardsInPlay.push(cards[cardID]);
+    checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+function checkForMatch(){
+    if (cardsInPlay.length ===2){
+        if(cardsInPlay[0] === cardsInPlay[1]){
+            alert("You've found a match");
+        }
+        else{
+            alert("Sorry try again!");
+        }
     }
-    else{
-        alert("Sorry try again!");
-    }
-
 }
